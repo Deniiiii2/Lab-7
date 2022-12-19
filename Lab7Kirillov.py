b89d3c1c2316e74612ed9d8791ebcafcf72d5fdf
Deniiiii2 <113837843+Deniiiii2@users.noreply.github.com>
2022-12-19 15:25:21 +0500

    Create Lab7Kirillov.py

---
+++ Lab7Kirillov.py
@@ -0,0 +1,51 @@
+k = int(input('k: '))
+l = int(input('l: '))
+m = int(input('m: '))
+n = int(input('n: '))
+
+figure = input('Выберите номер фигуры :\n1 Ферзь\n2 Ладья\n3 Слон\n4 Конь\n')
+# if (figure!='1' or figure!='2' or figure!='E' or figure!='H'):
+#   print('Некорректные значения')
+#  exit()
+
+if ((figure == '1' and (k == m or l == n or k - l == m - n or k + l == m + n)) or
+        (figure == '2' and (k == m or l == n)) or
+        (figure == '3' and (k - l == m - n or k + l == m + n)) or
+        (figure == '4' and ((abs(k - m) == 1 and abs(l - n) == 2) or (abs(k - m) == 2 and abs(l - n) == 1)))):
+    attack = 1
+
+else:
+    attack = 0
+figure2 = input('Выберите номер фигуры для второй задачи ) \n1 Ферзь\n2 Ладья\n3 Слон\n')
+
+if (k + l + m + n) % 2 == 0:
+    print('а) Поле', (k, l), 'и поле', (m, n), 'одного цвета')
+else:
+    print('а) Поле', (k, l), 'и поле', (m, n), 'не одного цвета')
+
+if attack == 1:
+    print('б) Фигура', figure, 'с координатами', (k, l), 'угрожает полю', (m, n))
+else:
+    print('б) Фигура', figure, ' с координатами', (k, l), 'не угрожает полю', (m, n))
+
+if ((figure2 == '1' and (k == m or l == n or k - l == m - n or k + l == m + n)) or
+        (figure2 == '2' and (k == m or l == n)) or  # Ладья по горизонтали, вертикали
+        (figure2 == '3' and (k - l == m - n or k + l == m + n))):  # Слон по диагонали
+    print('в) Фигура', figure2, 'с координатами', (k, l), 'может одним ходом попасть на поле', (m, n))
+else:
+    print('в) Фигура', figure2, 'с координатами', (k, l), 'не может одним ходом попасть на поле', (m, n))
+
+    if figure2 == 1 or figure2 == 2:  # для ферзя и ладьи это ход по вертикали
+        print('Можно попасть за два хода. Следующий ход на клетку:', (k, n))
+    elif figure2 == 3:
+        if (k + l + m + n) % 2 == 0:
+            for i in range(1, 9):
+                for o in range(1, 9):  # Перебор клеток
+                    if (i == k and o == l) or (i == m and o == n):
+                        continue
+                    if (k - l == i - o or k + l == i + o) and (
+                            i - o == m - n or i + o == m + n):  # если текущая клетка досягаема для слона
+                        print('Можно попасть за два хода. Следующий ход на клетку:', (i, o))
+                        break
+        else:  # Слон не может перейти на другой цвет
+            print('в) Фигура', figure2, 'на', (k, l), 'не может попасть на поле', (m, n))
